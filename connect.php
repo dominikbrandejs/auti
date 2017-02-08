a41a9552552ae297c5250eb66891f1631a1ecfe5
Dominik Brandejs <dominik.brandejs5@gmail.com>
2017-02-08 13:09:23 +0100

    Povezali smo s bazom podataka.

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$mysqli = new mysqli('172.0.0.1', 'tsd', 'Mint12345', 'auti');
+
+if ($mysqli ->connect_errno) {
+    exit;
+
+}
+?>
--- empty.php
+++ empty.php
@@ -1,3 +1,4 @@
+<?php require('connetc.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
